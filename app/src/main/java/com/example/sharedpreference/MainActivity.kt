94084f36c730e67d49adefb25589022d2f8cad77
akashtripathi0109 <57222581+akashtripathi0109@users.noreply.github.com>
2023-04-04 13:29:02 +0530

    MainActivity and activity xml

---
+++ app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.sharedpreference
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    lateinit var name : EditText
+    lateinit var emailId : EditText
+    lateinit var save : Button
+    lateinit var sharedPreferences: SharedPreferences
+    val Name = "nameKey" //key1
+    val Email = "emailKey"  //key2
+    val myFile = "mypreFile" //preferenceFile
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        name=findViewById(R.id.nameEdit)
+        emailId=findViewById(R.id.emailEdit)
+        save=findViewById(R.id.saveBtn)
+        sharedPreferences=getSharedPreferences(myFile,Context.MODE_PRIVATE)
+    }
+    fun save(v: View){
+        val n=name.text.toString()
+        val e=emailId.text.toString()
+        val editor = sharedPreferences.edit()
+        editor.putString(Name,n)
+        editor.putString(Email,e)
+        editor.apply()
+    }
+}
